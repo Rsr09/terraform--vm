6de15298b3c7a1150557d45fa51f34e7b6898b4e
Rajveer Singh Rajput <167510442+Rsr09@users.noreply.github.com>
2025-07-24 12:12:37 +0530

    Update 04 main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "azurerm_network_interface" "nic" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 
